6e5c01b83822004129e3286347cbd42db7940dc8
Pedro Gomes <pedrodemargomes@gmail.com>
2018-05-15 18:40:47 -0300

    a[

--- swParalelo.c
+++ swParalelo.c
@@ -12,35 +12,35 @@
 #define MAX_MATRIX MAX+1
 
 struct Maior { 
-	long int val; 
-	long int i;
-	long int j;
+	int val; 
+	int i;
+	int j;
 };    
 #pragma omp declare reduction(maximum : struct Maior : omp_out = omp_in.val > omp_out.val ? omp_in : omp_out)
 
-long int numSeq1,numSeq2;
+int numSeq1,numSeq2;
 char *seq1,*seq2;
-long int *M; //[MAX_MATRIX][MAX_MATRIX];
+int *M; //[MAX_MATRIX][MAX_MATRIX];
 
-long int numProcs;
+int numProcs;
 struct Maior maior;
 
 char *respSeq1;
 char *respSeq2;
 
-inline long int max(long int a, long int b) {
+inline int max(int a, int b) {
 	if(a > b)
 		return a;
 	return b;
 }
 
-inline long int min(long int a, long int b) {
+inline int min(int a, int b) {
 	if(a > b)
 		return b;
 	return a;
 }
 
-inline long int numElementosDiagonal(long int i, long int numSeq1, long int numSeq2) {
+inline int numElementosDiagonal(int i, int numSeq1, int numSeq2) {
 	if( i < numSeq1+1 && i < numSeq2+1 )
 		return i;
 	else if(i < max(numSeq1+1,numSeq2+1) )
@@ -49,7 +49,7 @@ inline long int numElementosDiagonal(long int i, long int numSeq1, long int numS
 		return 2*min(numSeq1+1,numSeq2+1) - i + abs(numSeq1+1 -(numSeq2+1) ) - 2;
 }
 
-inline void calcPrimElemDiagonal(long int i,long int *pi,long int *pj,long int numSeq1) {
+inline void calcPrimElemDiagonal(int i,int *pi,int *pj,int numSeq1) {
 	if (i < numSeq1+1) {
 		*pi = i;
  		*pj = 1;
@@ -59,7 +59,7 @@ inline void calcPrimElemDiagonal(long int i,long int *pi,long int *pj,long int n
 	}
 }
 
-inline void score(long int bi, long int bj){
+inline void score(int bi, int bj){
 	if(seq1[bi-1] == seq2[bj-1])
 		M[bi*(numSeq2+1)+bj] = M[(bi-1)*(numSeq2+1)+bj-1] + MATCH;
 	else
@@ -78,13 +78,13 @@ inline void score(long int bi, long int bj){
 }
 
 int main() {
-	numProcs= 20;//omp_get_num_procs();
+	numProcs= 100;//omp_get_num_procs();
 
-	long int i,j;
+	int i,j;
 
 	maior.val = 0;
 
-	scanf("%ld %ld",&numSeq1,&numSeq2);
+	scanf("%d %d",&numSeq1,&numSeq2);
 	
 	seq1 = malloc(numSeq1*sizeof(char));
 	seq2 = malloc(numSeq2*sizeof(char));
@@ -94,22 +94,22 @@ int main() {
 	scanf("%s",seq1);
 	scanf("%s",seq2);
 
-	M = calloc( (numSeq1+1) * (numSeq2+1), sizeof(long int));
+	M = calloc( (numSeq1+1) * (numSeq2+1), sizeof(int));
 
 
-	long int s_block = ceil((double)numSeq2/numProcs);
-	long int bi,bj; // i e d do bloco
+	int s_block = ceil((double)numSeq2/numProcs);
+	int bi,bj; // i e d do bloco
 
-	long int pi,pj; // Primeiros i e j da diagonal
-	long int ki,kj;
-	long int numDiagonais = numSeq1/s_block+numSeq2/s_block-1; // Numero de diagonais a percorrer
-	long int numElementos; // Numero de elementos na diagonal
+	int pi,pj; // Primeiros i e j da diagonal
+	int ki,kj;
+	int numDiagonais = numSeq1/s_block+numSeq2/s_block-1; // Numero de diagonais a percorrer
+	int numElementos; // Numero de elementos na diagonal
 	
 	for(i =1; i <= numDiagonais; i++) {
 		numElementos = numElementosDiagonal(i,numSeq1/s_block,numSeq2/s_block);
 		calcPrimElemDiagonal(i,&pi,&pj,numSeq1/s_block);
 		
-		#pragma omp parallel for private(ki,kj,bi,bj) reduction(maximum:maior)
+		#pragma omp parallel for private(ki,kj,bi,bj) reduction(maximum:maior) num_threads(8)
 		for(j = 1;j <= numElementos; j++) {
 			
 			ki = pi - j +1;
@@ -156,21 +156,21 @@ int main() {
 	#ifdef DEBUG
 	for(i=0;i<numSeq1+1;i++) {
 		for(j=0;j<numSeq2+1;j++) {
-			printf("%ld ",M[i*(numSeq2+1)+j]);
+			printf("%d ",M[i*(numSeq2+1)+j]);
 		}
 		printf("\n");
 	}
 	#endif
 
 	#ifdef DEBUG
-	printf("i: %ld j: %ld maior: %ld\n", maior.i,maior.j,maior.val);
+	printf("i: %d j: %d maior: %d\n", maior.i,maior.j,maior.val);
 	#endif
 	
 
 	int count = 0;
 	i = maior.i; j = maior.j;
 	while(M[i*(numSeq2+1)+j] != 0) {
-		if( M[(i-1)*(numSeq2+1)+j-1] >= M[(i-1)*(numSeq2+1)+j] && M[(i-1)*(numSeq2+1)+j-1] >= M[i*(numSeq2+1)+j-1] ) {
+		if( M[(i-1)*(numSeq2+1)+j-1] > M[(i-1)*(numSeq2+1)+j] && M[(i-1)*(numSeq2+1)+j-1] >= M[i*(numSeq2+1)+j-1] ) {
 			respSeq1[count] = seq1[i-1];
 			respSeq2[count] = seq2[j-1];
 			i = i-1;j = j-1; // Diagonal
--- swSerial.c
+++ swSerial.c
@@ -10,7 +10,7 @@
 #define MAX_MATRIX MAX+1
 
 char *seq1,*seq2;
-long int *M;
+int *M;
 
 char *respSeq1;
 char *respSeq2;
@@ -18,12 +18,12 @@ char *respSeq2;
 int main() {
 	
 
-	long int i,j;
-	long int maiorI,maiorJ,maior;
+	int i,j;
+	int maiorI,maiorJ,maior;
 	maior = 0;
 
-	long int numSeq1,numSeq2;
-	scanf("%ld %ld",&numSeq1,&numSeq2);
+	int numSeq1,numSeq2;
+	scanf("%d %d",&numSeq1,&numSeq2);
 
 	seq1 = malloc(numSeq1*sizeof(char));
 	seq2 = malloc(numSeq2*sizeof(char));
@@ -33,7 +33,7 @@ int main() {
 	scanf("%s",seq1);
 	scanf("%s",seq2);
 
-	M = calloc( (numSeq1+1) * (numSeq2+1), sizeof(long int));
+	M = calloc( (numSeq1+1) * (numSeq2+1), sizeof(int));
 
 	/*numSeq1 = 9;
 	numSeq2 = 8;
@@ -41,18 +41,12 @@ int main() {
 	char *seq2 = "TGTTACGG";*/
 
 
-	M[0] = 0;
-	for(i=0;i<numSeq1+1;i++) {
-		for(j=0;j<numSeq2+1;j++) {
-			M[i*(numSeq2+1)+j] = 0;
-		}
-	}
-
 	for(i=1;i<numSeq1+1;i++) {
 		for(j=1;j<numSeq2+1;j++) {
+			//printf("(%d,%d): %c %c\n",i,j,seq1[i-1], seq2[j-1] );
 			if(seq1[i-1] == seq2[j-1])
 				M[i*(numSeq2+1)+j] = M[(i-1)*(numSeq2+1)+j-1] + MATCH;
-			else
+			else 
 				M[i*(numSeq2+1)+j] = M[(i-1)*(numSeq2+1)+j-1] + MISS;
 			if(M[i*(numSeq2+1)+j] < M[(i-1)*(numSeq2+1)+j] + PENALTY )
 				M[i*(numSeq2+1)+j] = M[(i-1)*(numSeq2+1)+j] + PENALTY;
@@ -70,21 +64,21 @@ int main() {
 	#ifdef DEBUG
 	for(i=0;i<numSeq1+1;i++) {
 		for(j=0;j<numSeq2+1;j++) {
-			printf("%ld ",M[i*(numSeq2+1)+j]);
+			printf("%u ",M[i*(numSeq2+1)+j]);
 		}
 		printf("\n");
 	}
 	#endif
 
 	#ifdef DEBUG
-	printf("i: %ld j: %ld maior: %ld\n", maiorI,maiorJ,maior);
+	printf("i: %d j: %d maior: %d\n", maiorI,maiorJ,maior);
 	#endif
 	
 
-	long int count = 0;
+	int count = 0;
 	i = maiorI; j = maiorJ;
 	while(M[i*(numSeq2+1)+j] != 0) {
-		if( M[(i-1)*(numSeq2+1)+j-1] >= M[(i-1)*(numSeq2+1)+j] && M[(i-1)*(numSeq2+1)+j-1] >= M[i*(numSeq2+1)+j-1] ) {
+		if( M[(i-1)*(numSeq2+1)+j-1] > M[(i-1)*(numSeq2+1)+j] && M[(i-1)*(numSeq2+1)+j-1] >= M[i*(numSeq2+1)+j-1] ) {
 			respSeq1[count] = seq1[i-1];
 			respSeq2[count] = seq2[j-1];
 			i = i-1;j = j-1; // Diagonal
